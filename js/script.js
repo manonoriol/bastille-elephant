48684ca1c7cf22caa3ba7e8907615b765ba58dc9
Manon Oriol <oriol.manon@gmail.com>
2022-10-26 20:12:16 +0200

    basic horizontal scroll ok

---
+++ js/script.js
@@ -0,0 +1,18 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".slide");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "power3",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  }
+});
+
+console.log('poopie');
