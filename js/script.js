087cf09684b40578d0a75d6462143c13802f4046
Manon Oriol <oriol.manon@gmail.com>
2022-11-17 23:51:20 +0100

    front title ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+//slide 1 black mask animation
+
+var tl1 = new TimelineMax;
+
+tl1.to('.mask', 2.8, {y:'-100vh', ease: Power2.easeInOut}, 2.5)
+
 // gsap switch div horizontal
 
 gsap.registerPlugin(ScrollTrigger);
@@ -6,16 +12,48 @@ let sections = gsap.utils.toArray(".slide");
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
-  ease: "power3",
+  ease: "none",
   scrollTrigger: {
     trigger: ".container",
     pin: true,
     scrub: 1,
     snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: "+=3500",
+    end: () => "+=" + document.querySelector(".container").offsetWidth
   }
 });
+
+// go back btn
+
+const btn = document.getElementById('icon-s12');
+
+btn.addEventListener('click', goBack);
+
+function goBack() {
+gsap.to(Window, {duration: 3, scrollTo: {x: "min"}, ease: "power3"});
+console.log("clicked");
+};
+
+//title animation gsap
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl2 = gsap.timeline({delay: 1});
+
+tl2.from('.main-title', {
+    y: 40,
+    opacity: 0,
+    duration: 2,
+    ease: 'power4'
+})  
+
+let tl3 = gsap.timeline({delay: 1.6});
+
+tl3.from('.sub-title', {
+    y: 40,
+    opacity: 0,
+    duration: 2,
+    ease: 'power4'
+})  
 
 // vanilla js scroll down for x scroll
 
@@ -29,16 +67,3 @@ function transformScroll(event) {
 
 var element = document.scrollingElement || document.documentElement;
 element.addEventListener('wheel', transformScroll);
-
-// go back btn
-
-window.onload=function(){
-  const btn = document.getElementById('icon-s12');
-
-  btn.addEventListener('click', goBack);
-  
-  function goBack() {
-    gsap.to(window, {duration: 3, scrollTo: {x: "min"}, ease: "power3"});
-  };
-}
-
