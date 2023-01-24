4f90a21392a1658828f3192617da1329ef419758
Manon Oriol <oriol.manon@gmail.com>
2023-01-24 23:30:14 +0100

    js media query for desktops added

--- js/script.js
+++ js/script.js
@@ -1,160 +1,167 @@
-//slide 1 black mask animation
+const mqDesktops = window.matchMedia("(min-width: 1200px)");
 
-let tl1 = new TimelineMax;
+window.onload = function() {
+  if(mqDesktops.matches) {
+    
+    //slide 1 black mask animation
 
-tl1.to('.mask', 2.8, {y:'-100vh', ease: Power2.easeInOut}, 2.5)
+    let tl1 = new TimelineMax;
 
-// gsap switch div horizontal
+    tl1.to('.mask', 2.8, {y:'-100vh', ease: Power2.easeInOut}, 2.5)
 
-gsap.registerPlugin(ScrollTrigger);
+    // gsap switch div horizontal
 
-// ScrollTrigger.normalizeScroll(true);
+    gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".slide");
+    // ScrollTrigger.normalizeScroll(true);
 
-let scrollTween = gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none", // <-- IMPORTANT!
-  scrollTrigger: {
-    trigger: ".container",
-    start: "top top",
-    end: "+=3000",
-    pin: true,
-    scrub: 1.5
+    let sections = gsap.utils.toArray(".slide");
+
+    let scrollTween = gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none", // <-- IMPORTANT!
+      scrollTrigger: {
+        trigger: ".container",
+        start: "top top",
+        end: "+=3000",
+        pin: true,
+        scrub: 1.5
+      }
+    });
+
+    // go back btn
+
+    // gsap.registerPlugin(ScrollToPlugin);
+
+    // const btn = document.getElementById('icon-s12');
+
+    // btn.addEventListener('click', goBack);
+
+    // function goBack() {
+    // gsap.to(window, {duration: 3, scrollTo: 200, ease: "power3"});
+    // console.log("clicked");
+    // };
+
+    const btn = document.getElementById('icon-s12');
+
+    btn.addEventListener("click", () => {
+      window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+      });
+    });
+
+    //main title animation gsap
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    let tl2 = gsap.timeline({delay: 1});
+
+    tl2.from('.main-title', {
+        y: 40,
+        opacity: 0,
+        duration: 2,
+        ease: 'power4'
+    })  
+
+    let tl3 = gsap.timeline({delay: 1.6});
+
+    tl3.from('.sub-title', {
+        y: 40,
+        opacity: 0,
+        duration: 2,
+        ease: 'power4'
+    })  
+
+    // vanilla js scroll down for x scroll
+
+    function transformScroll(event) {
+      if (!event.deltaY) {
+        return;
+      }
+
+      event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+    }
+
+    var element = document.scrollingElement || document.documentElement;
+    element.addEventListener('wheel', transformScroll);
+
+    // section-titles animation
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const titles = gsap.utils.toArray(".section-title");
+
+    titles.forEach((title, i) => {
+      gsap.from(title, {
+        y: 10,
+        opacity: 0,
+        duration: 1,
+        ease: "power1",
+        scrollTrigger: {
+          trigger: "." + title.parentElement.parentElement.className.slice(6),
+          containerAnimation: scrollTween,
+          start: "left center",
+          toggleActions: "play none none pause"
+        }
+      });
+    });
+
+    // paragraphs animation
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const paragraphs = gsap.utils.toArray(".paragraph");
+
+    paragraphs.forEach((para, i) => {
+      gsap.from(para, {
+        y: 5,
+        opacity: 0,
+        duration: 2,
+        ease: "power1",
+        scrollTrigger: {
+          trigger: "." + para.parentElement.parentElement.className.slice(6),
+          containerAnimation: scrollTween,
+          start: "left center",
+          toggleActions: "play none none pause"
+        }
+      });
+    });
+
+    // paris animation
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const paris = new SplitType('#paris');
+
+    gsap.to('.char', {
+      y: 0,
+      stagger: 0.2,
+      delay: 0.2,
+      duration: .4,
+      ease: "power2",
+      scrollTrigger: {
+        trigger:'.slide-2',
+        containerAnimation: scrollTween,
+        start: "left center",
+        toggleActions: "play none none pause"
+      }
+    });
+
+    // slide-5 animation
+
+    gsap.to('.date', {
+      y: 0,
+      stagger: 0.4,
+      delay: 0.2,
+      duration: .4,
+      ease: "power2",
+      scrollTrigger: {
+        trigger:'.slide-5',
+        containerAnimation: scrollTween,
+        start: "left center",
+        toggleActions: "play none none pause"
+      }
+    });
   }
-});
-
-// go back btn
-
-// gsap.registerPlugin(ScrollToPlugin);
-
-// const btn = document.getElementById('icon-s12');
-
-// btn.addEventListener('click', goBack);
-
-// function goBack() {
-// gsap.to(window, {duration: 3, scrollTo: 200, ease: "power3"});
-// console.log("clicked");
-// };
-
-const btn = document.getElementById('icon-s12');
-
-btn.addEventListener("click", () => {
-  window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-  });
-});
-
-//main title animation gsap
-
-gsap.registerPlugin(ScrollTrigger);
-
-let tl2 = gsap.timeline({delay: 1});
-
-tl2.from('.main-title', {
-    y: 40,
-    opacity: 0,
-    duration: 2,
-    ease: 'power4'
-})  
-
-let tl3 = gsap.timeline({delay: 1.6});
-
-tl3.from('.sub-title', {
-    y: 40,
-    opacity: 0,
-    duration: 2,
-    ease: 'power4'
-})  
-
-// vanilla js scroll down for x scroll
-
-function transformScroll(event) {
-  if (!event.deltaY) {
-    return;
-  }
-
-  event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
 }
-
-var element = document.scrollingElement || document.documentElement;
-element.addEventListener('wheel', transformScroll);
-
-// section-titles animation
-
-gsap.registerPlugin(ScrollTrigger);
-
-const titles = gsap.utils.toArray(".section-title");
-
-titles.forEach((title, i) => {
-  gsap.from(title, {
-    y: 10,
-    opacity: 0,
-    duration: 1,
-    ease: "power1",
-    scrollTrigger: {
-      trigger: "." + title.parentElement.parentElement.className.slice(6),
-      containerAnimation: scrollTween,
-      start: "left center",
-      toggleActions: "play none none pause"
-    }
-  });
-});
-
-// paragraphs animation
-
-gsap.registerPlugin(ScrollTrigger);
-
-const paragraphs = gsap.utils.toArray(".paragraph");
-
-paragraphs.forEach((para, i) => {
-  gsap.from(para, {
-    y: 5,
-    opacity: 0,
-    duration: 2,
-    ease: "power1",
-    scrollTrigger: {
-      trigger: "." + para.parentElement.parentElement.className.slice(6),
-      containerAnimation: scrollTween,
-      start: "left center",
-      toggleActions: "play none none pause"
-    }
-  });
-});
-
-// paris animation
-
-gsap.registerPlugin(ScrollTrigger);
-
-const paris = new SplitType('#paris');
-
-gsap.to('.char', {
-  y: 0,
-  stagger: 0.2,
-  delay: 0.2,
-  duration: .4,
-  ease: "power2",
-  scrollTrigger: {
-    trigger:'.slide-2',
-    containerAnimation: scrollTween,
-    start: "left center",
-    toggleActions: "play none none pause"
-  }
-});
-
-// slide-5 animation
-
-gsap.to('.date', {
-  y: 0,
-  stagger: 0.4,
-  delay: 0.2,
-  duration: .4,
-  ease: "power2",
-  scrollTrigger: {
-    trigger:'.slide-5',
-    containerAnimation: scrollTween,
-    start: "left center",
-    toggleActions: "play none none pause"
-  }
-});
